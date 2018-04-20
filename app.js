c842a68d0a5840dcb5448ec92b2a953b68484d87
jjade seravla <jadealvares@hotmail.co.uk>
2018-04-20 14:24:37 +0100

    using leaflet.js to display map on html page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ fs.readFile('index.html', (err, html) => {
   if(err){
     throw err;
   }
+
   const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-type', 'text/html');
